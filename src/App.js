3c64d4a13b39ebb69fa2c869bb6be7fad9603132
sunil manthena <sunilmanthena2601@gmail.com>
2022-06-24 10:10:51 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Seprated link</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
